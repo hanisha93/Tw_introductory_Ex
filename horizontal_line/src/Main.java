4a5a23f7482e3d3acccc11d5234ab5bfb2f42d1a
Hanisha <hanishap@thoughtworks>
2015-05-30 13:10:07 +0530

    horizontally printed asteriks

---
+++ horizontal_line/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        int n;
+
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter number");
+        n = in.nextInt();
+        for(int i=0;i<n;i++)
+        System.out.print("*");
+
+
+    }
+}
